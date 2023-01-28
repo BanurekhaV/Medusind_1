d0f13aadeae896ef2bb39a12e7a0d9c4f98395a8
BanurekhaV <rekhasairam@hotmail.com>
2023-01-28 19:53:52 +0530

    Assignment 3

---
+++ CSharp/Day4CSharpprj/Day4CSharpprj/ExceptionEg.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4CSharpprj
+{
+    class ExceptionEg
+    {
+        static void Main()
+        {
+            int a, b, c;
+            try
+            {
+                Console.WriteLine("enter 2 nos :");
+                a = int.Parse(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+                int[] arr = { 2, 4, 6, 8 };
+                Console.WriteLine(arr[2]);
+                Console.WriteLine(c);
+
+            }           
+            catch (FormatException fe)
+            {
+                //Console.WriteLine(fe.Message + fe.StackTrace+ fe.Source);
+                Console.WriteLine("Please enter only numbers");
+            }
+            catch (DivideByZeroException de)
+            {
+                Console.WriteLine("You cannot divide a number by Zero");
+            }
+            catch(IndexOutOfRangeException ie)
+            {
+                Console.WriteLine("Attempting to read values beyond array");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops !! some error occurred. Try Later");
+            }
+
+            finally{
+                Console.WriteLine("Reached Finally");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day4CSharpprj/Day4CSharpprj/PropertiesEg.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4CSharpprj
+{
+    class Students
+    {
+        private string code = "N.A.";
+        string name = "Not Known";
+        int age = 0;
+
+        //declare properties
+        //1. for code field only get accessor
+        public string _code
+        {
+            get { return code; }
+        }
+
+        //2. for name field
+        public string stdname
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        //3. for age field
+        public int Age
+        {
+            get { return age; }
+            set { if (value <= 20) { age = 21; } else age = value; }
+        }
+
+        //overiding toString()
+
+        public override string ToString()
+        {
+            return "Code = " + " " +code + "Name = " + " "+ name + "Age = " + age;
+        }
+    }
+    class PropertiesEg
+    {
+        static void Main()
+        {
+            //create a students object
+            Students student = new Students();
+            // student._code = "S001"; read only property
+            Console.WriteLine($"{student._code}, {student.stdname}, {student.Age}");
+            student.stdname = "Priya";
+            student.Age = 14;
+            Console.WriteLine($"{student._code}, {student.stdname}, {student.Age}");
+            student.Age += 2;
+            Console.WriteLine("Student Info {0}",student.ToString());
+            Console.Read();
+        }
+    }
+
+    //automatic properties 
+    class Product
+    {
+        public int ProductId { get; private set; }
+        public string ProductName { get; protected set; }
+        public float ProductPrice { get; }
+        public DateTime Expiry { get; } = Convert.ToDateTime("31/12/2023");
+    }
+}
